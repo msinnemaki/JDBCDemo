15cb756327f155474aea909fd9ec22d6c4ac3580
msinnemaki <matt.sinnemaki@gmail.com>
2022-06-30 11:48:43 -0600

    adding demo to repository

--- JDBC-Demo/src/main/java/Main.java
+++ JDBC-Demo/src/main/java/Main.java
@@ -1,2 +1,20 @@
-public class Main {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Connection connection = ConnectionManager.connect();
+
+        try
+        {
+            System.out.println(connection.getClientInfo().isEmpty());
+
+        }
+        catch (SQLException e)
+        {
+
+        }
+    }
 }
